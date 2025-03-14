c261a04bdc75f19918ca082866538875071c37e3
agorski1 <artur.gorski5@wp.pl>
2025-03-15 00:55:59 +0100

    Entity

---
+++ r1/Entity.cpp
@@ -0,0 +1,71 @@
+﻿#include "Entity.h"
+#include <cassert>
+#include <iostream>
+
+Entity::Entity(int hitpoints)
+	: mVelocity()
+	, mHitpoints(hitpoints)
+{
+}
+
+
+void Entity::damage(int points)
+{
+	assert(points > 0);
+
+	mHitpoints -= points;
+}
+
+void Entity::destroy()
+{
+	mHitpoints = 0;
+}
+
+void Entity::remove()
+{
+	destroy();
+}
+
+int Entity::getHitpoints() const
+{
+	return mHitpoints;
+}
+
+bool Entity::isDestroyed() const
+{
+	return mHitpoints <= 0;
+}
+
+void Entity::setVelocity(sf::Vector2f velocity)
+{
+	mVelocity = velocity;
+}
+
+void Entity::setVelocity(float vx, float vy)
+{
+	mVelocity.x = vx;
+	mVelocity.y = vy;
+}
+
+void Entity::accelerate(sf::Vector2f velocity)
+{
+	mVelocity += velocity;
+}
+
+void Entity::accelerate(float vx, float vy)
+{
+	mVelocity.x += vx;
+	mVelocity.y += vy;
+}
+
+sf::Vector2f Entity::getVelocity() const
+{
+	return mVelocity;
+}
+
+void Entity::updateCurrent(sf::Time dt, CommandQueue& commands)
+{
+	move(mVelocity * dt.asSeconds());
+}
+
+
---
+++ r1/Entity.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "SFML/System/Time.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SceneNode.h"
+
+class CommandQueue;
+
+class Entity : public SceneNode
+{
+public:	
+	explicit Entity(int hitpoints);
+	void damage(int points);
+	void destroy();
+	void remove();
+
+	int getHitpoints() const;
+	virtual bool isDestroyed() const;
+
+	void setVelocity(sf::Vector2f velocity);
+	void setVelocity(float vx, float vy);
+
+	void accelerate(sf::Vector2f velocity);
+	void accelerate(float vx, float vy);
+
+
+	sf::Vector2f getVelocity() const;
+
+protected:
+	virtual void updateCurrent(sf::Time dt, CommandQueue& commands);
+private:
+	sf::Vector2f mVelocity;
+	int mHitpoints;
+};
+
