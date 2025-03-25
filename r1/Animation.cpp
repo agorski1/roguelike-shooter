48f5f09a5f8c2195672c4fc2bc5c4a2ed0d324fc
agorski1 <artur.gorski5@wp.pl>
2025-03-26 00:00:48 +0100

    Animation v2

--- r1/Animation.cpp
+++ r1/Animation.cpp
@@ -1,138 +1,161 @@
-#include "Animation.h"
+﻿#include "Animation.h"
+#include "SFML/Graphics/RenderTarget.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include "DataTables.h"
+#include <iostream>
 
-void Animation::update(sf::Time dt)
+namespace
 {
-	sf::Time timePerFrame = mDuration / static_cast<float>(mNumFrames);
-	mElapsedTime += dt;
-
-	sf::Vector2i textureBounds(mSprite.getTexture()->getSize());
-	sf::IntRect textureRect = mSprite.getTextureRect();
-
-	if (mCurrentFrame == 0)
-		textureRect = sf::IntRect(0, 0, mFrameSize.x, mFrameSize.y);
-
-	while (mElapsedTime >= timePerFrame && (mCurrentFrame <= mNumFrames || mRepeat))
-	{
-		textureRect.left += textureRect.width;
-
-		if (textureRect.left + textureRect.width > textureBounds.x)
-		{
-			textureRect.left = 0;
-			textureRect.top += textureRect.height;
-		}
-
-		mElapsedTime -= timePerFrame;
-		if (mRepeat)
-		{
-			mCurrentFrame = (mCurrentFrame + 1) % mNumFrames;
-
-			if (mCurrentFrame == 0)
-				textureRect = sf::IntRect(0, 0, mFrameSize.x, mFrameSize.y);
-		}
-		else
-		{
-			mCurrentFrame++;
-		}
-	}
-
-	mSprite.setTextureRect(textureRect);
+    const std::vector<AnimationData> Table = initializeAnimationData();
 }
 
-void Animation::draw(sf::RenderTarget& target, sf::RenderStates states) const
+Animation::Animation(EntityType entityType)
+    : mEntityType(entityType)
+    , mSprite()
+    , mCurrentType(AnimationType::IdleDown)
+    , mCurrentFrame(0)
+    , mElapsedTime(sf::Time::Zero)
+    , mFrameSize(Table[mEntityType].frameSize)
+    , mDuration(Table[mEntityType].duration)
+    , mRepeat(Table[mEntityType].repeat)
+    , mFrameCountMap(Table[mEntityType].frameCounts)
 {
-	states.transform *= getTransform();
-	target.draw(mSprite, states);
 }
 
-Animation::Animation()
-	: mSprite()
-	, mFrameSize()
-	, mNumFrames(0)
-	, mCurrentFrame(0)
-	, mDuration(sf::Time::Zero)
-	, mElapsedTime(sf::Time::Zero)
-	, mRepeat(false)
+Animation::Animation(const sf::Texture& texture, EntityType entityType)
+    : Animation(entityType)
 {
-
-}
-
-Animation::Animation(const sf::Texture& texture)
-	: mSprite(texture)
-	, mFrameSize()
-	, mNumFrames(0)
-	, mCurrentFrame(0)
-	, mDuration(sf::Time::Zero)
-	, mElapsedTime(sf::Time::Zero)
-	, mRepeat(false)
-{
+    setTexture(texture);
 }
 
 void Animation::setTexture(const sf::Texture& texture)
 {
-	mSprite.setTexture(texture);
+    mSprite.setTexture(texture);
 }
 
 const sf::Texture* Animation::getTexture() const
 {
-	return mSprite.getTexture();
+    return mSprite.getTexture();
 }
 
 void Animation::setFrameSize(sf::Vector2i frameSize)
 {
-	mFrameSize = frameSize;
+    mFrameSize = frameSize;
 }
 
 sf::Vector2i Animation::getFrameSize() const
 {
-	return mFrameSize;
-}
-
-void Animation::setNumFrames(std::size_t numFrames)
-{
-	mNumFrames = numFrames;
-}
-
-std::size_t Animation::getNumFrames() const
-{
-	return mNumFrames;
+    return mFrameSize;
 }
 
 void Animation::setDuration(sf::Time duration)
 {
-	mDuration = duration;
+    mDuration = duration;
 }
 
 sf::Time Animation::getDuration() const
 {
-	return mDuration;
+    return mDuration;
 }
 
 void Animation::setRepeating(bool flag)
 {
-	mRepeat = flag;
+    mRepeat = flag;
 }
 
 bool Animation::isRepeating() const
 {
-	return mRepeat;
+    return mRepeat;
 }
 
 void Animation::restart()
 {
-	mCurrentFrame = 0;
+    mCurrentFrame = 0;
+    mElapsedTime = sf::Time::Zero;
 }
 
 bool Animation::isFinished() const
 {
-	return mCurrentFrame >= mNumFrames;
+    return !mRepeat && mCurrentFrame >= getNumFramesForRow(mCurrentType);
+}
+
+void Animation::setAnimationType(AnimationType type)
+{
+    if (mCurrentType != type)
+    {
+        mCurrentType = type;
+        restart();
+    }
+}
+
+AnimationType Animation::getAnimationType() const
+{
+    return mCurrentType;
+}
+
+void Animation::setNumFramesForRow(AnimationType type, int numFrames)
+{
+    mFrameCountMap[type] = numFrames;
+}
+
+int Animation::getNumFramesForRow(AnimationType type) const
+{
+    auto it = mFrameCountMap.find(type);
+    if (it != mFrameCountMap.end())
+        return it->second;
+
+    return 1;
+}
+
+void Animation::update(sf::Time dt)
+{
+    // Aktualizujemy czas, który upłynął od ostatniej zmiany klatki
+    mElapsedTime += dt;
+
+    // Obliczamy czas trwania jednej klatki
+    int numFrames = getNumFramesForRow(mCurrentType);
+    if (numFrames <= 0) return;
+
+    sf::Time frameTime = mDuration / static_cast<float>(numFrames);
+
+    // Jeśli upłynął czas na zmianę klatki
+    while (mElapsedTime >= frameTime)
+    {
+        mElapsedTime -= frameTime;
+
+        // Przechodzimy do kolejnej klatki
+        if (mCurrentFrame + 1 < numFrames)
+        {
+            mCurrentFrame++;
+        }
+        else if (mRepeat) // Jeśli animacja się powtarza, wracamy do pierwszej klatki
+        {
+            mCurrentFrame = 0;
+        }
+
+        // Aktualizujemy teksturę, przesuwając "widok" sprite'a na odpowiednią klatkę
+        int frameWidth = mFrameSize.x;
+        int frameHeight = mFrameSize.y;
+        mSprite.setTextureRect(sf::IntRect(mCurrentFrame * frameWidth,
+            static_cast<int>(mCurrentType) * frameHeight,
+            frameWidth,
+            frameHeight));
+    }
+}
+
+
+void Animation::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+    target.draw(mSprite, states);
 }
 
 sf::FloatRect Animation::getLocalBounds() const
 {
-	return sf::FloatRect(getOrigin(), static_cast<sf::Vector2f>(getFrameSize()));
+    return sf::FloatRect(sf::Vector2f(), sf::Vector2f(mFrameSize));
 }
 
-sf::FloatRect Animation::getGloablBounds() const
+sf::FloatRect Animation::getGlobalBounds() const
 {
-	return getTransform().transformRect(getLocalBounds());
+    return getTransform().transformRect(getLocalBounds());
 }
--- r1/Animation.h
+++ r1/Animation.h
@@ -1,43 +1,73 @@
+﻿#pragma once
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Graphics/Drawable.hpp"
+#include "SFML/Graphics/Transformable.hpp"
+#include "SFML/System/Time.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+
 #pragma once
-#include "SFML/Graphics.hpp"
+
+#include <SFML/Graphics.hpp>
+#include <unordered_map>
+
+enum class AnimationType
+{
+    IdleUp, IdleLeft, IdleDown, IdleRight,
+    WalkUp, WalkLeft, WalkDown, WalkRight,
+    Dash,
+};
 
 class Animation : public sf::Drawable, public sf::Transformable
 {
 public:
-	Animation();
-	explicit Animation(const sf::Texture& texture);
-	void setTexture(const sf::Texture& texture);
-	const sf::Texture* getTexture() const;
+    enum EntityType
+    {
+        Player,
+        Enemy,
+        NPC,
+        Pickup,
+        EntityCount,
+    };
 
-	void setFrameSize(sf::Vector2i frameSize);
-	sf::Vector2i getFrameSize() const;
-	void setNumFrames(std::size_t numFrames);
-	std::size_t getNumFrames() const;
+    explicit Animation(EntityType entityType);
+    explicit Animation(const sf::Texture& texture, EntityType entityType);
 
-	void setDuration(sf::Time duration);
-	sf::Time getDuration() const;
+    void setTexture(const sf::Texture& texture);
+    const sf::Texture* getTexture() const;
 
-	void setRepeating(bool flag);
-	bool isRepeating() const;
+    void setFrameSize(sf::Vector2i frameSize);
+    sf::Vector2i getFrameSize() const;
 
-	void restart();
-	bool isFinished() const;
+    void setDuration(sf::Time duration);
+    sf::Time getDuration() const;
 
-	sf::FloatRect getLocalBounds() const;
-	sf::FloatRect getGloablBounds() const;
+    void setRepeating(bool flag);
+    bool isRepeating() const;
 
-	void update(sf::Time dt);
+    void restart();
+    bool isFinished() const;
+
+    void setAnimationType(AnimationType type);
+    AnimationType getAnimationType() const;
+
+    void setNumFramesForRow(AnimationType type, int numFrames);
+    int getNumFramesForRow(AnimationType type) const;
+
+    sf::FloatRect getLocalBounds() const;
+    sf::FloatRect getGlobalBounds() const;
+    void update(sf::Time dt);
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 private:
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    EntityType mEntityType;
+    sf::Sprite mSprite;
+    sf::Vector2i mFrameSize;
+    AnimationType mCurrentType;
+    int mCurrentFrame;
+    sf::Time mDuration;
+    sf::Time mElapsedTime;
+    bool mRepeat;
 
-private:
-	sf::Sprite mSprite;
-	sf::Vector2i mFrameSize;
-	std::size_t mNumFrames;
-	std::size_t mCurrentFrame;
-	sf::Time mDuration;
-	sf::Time mElapsedTime;
-	bool mRepeat;
+    std::unordered_map<AnimationType, int> mFrameCountMap; // Przechowuje liczbę klatek dla każdego AnimationType
 };
 
