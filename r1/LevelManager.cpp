07e5c0cd2f693b840d6220ecc2218529c89617ab
agorski1 <artur.gorski5@wp.pl>
2025-04-11 12:19:20 +0200

    LevelManager

---
+++ r1/LevelManager.cpp
@@ -0,0 +1,83 @@
+﻿#include "LevelManager.h"
+#include "DataTables.h"
+#include "EntityManager.h"
+
+namespace 
+{
+    LevelTable levelTable = initializeLevelData();
+}
+
+
+LevelManager::LevelManager(MapGenerator& mapGenerator, TileMap& tileMap, EntityManager& entityManager, SceneNode& sceneGraph)
+    : mMapGenerator(mapGenerator)
+    , mTileMap(tileMap)
+    , mEntityManager(entityManager)
+    , mSceneGraph(sceneGraph)
+    , mCurrentLevelIndex(0)
+    , mLevelCompleted(true)
+    , mRandom(static_cast<unsigned>(std::time(nullptr)))
+    , mLevelTable(levelTable)
+{
+}
+
+
+
+void LevelManager::generateLevel()
+{
+    bool isBossLevel = (mCurrentLevelIndex + 1) % 10 == 0;
+
+    LevelData levelData;
+    if (isBossLevel)
+    {
+        std::uniform_int_distribution<> dis(0, mLevelTable.bossLevels.size() - 1);
+        levelData = mLevelTable.bossLevels[dis(mRandom)];
+    }
+    else
+    {
+        std::uniform_int_distribution<> dis(0, mLevelTable.regularLevels.size() - 1);
+        levelData = mLevelTable.regularLevels[dis(mRandom)];
+    }
+
+    mMapGenerator.generateLevel(levelData);
+    mTileMap.load(mMapGenerator.getMap(), levelData.levelTextures);
+    //mEntityManager.spawnEntities(mTileMap, isBossLevel);
+}
+
+void LevelManager::update(sf::Time dt)
+{
+    if (!mLevelCompleted)
+    {
+        checkLevelCompletion();
+    }
+
+    if (mTileMap.hasPlayerEnteredPortal())
+    {
+        advanceToNextLevel();
+        mTileMap.resetPlayerEnteredPortal();
+    }
+}
+
+void LevelManager::checkLevelCompletion()
+{
+    if (mEntityManager.getEnemyCount() == 0)
+    {
+        mLevelCompleted = true;
+        std::cout << "portal" << std::endl;
+        mTileMap.spawnPortal();
+    }
+}
+
+void LevelManager::advanceToNextLevel()
+{
+    if (isGameCompleted())
+    {
+        return;
+    }
+
+    mCurrentLevelIndex++;
+    mLevelCompleted = false;
+
+
+    // watek i stan loading next level
+    generateLevel();
+}
---
+++ r1/LevelManager.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "DataTables.h"
+#include "MapGenerator.h"
+#include "TileMap.h"
+#include "SceneNode.h"
+#include <random>
+
+struct LevelTable;
+class EntityManager;
+
+
+class LevelManager
+{
+public:
+    LevelManager(MapGenerator& mapGenerator, TileMap& tileMap, EntityManager& entityManager, SceneNode& sceneGraph);
+
+    void update(sf::Time dt);
+    void checkLevelCompletion();
+    void advanceToNextLevel();
+
+    int getCurrentLevelIndex() const { return mCurrentLevelIndex; }
+    bool isGameCompleted() const { return mCurrentLevelIndex >= 50; }
+    void generateLevel();
+
+private:
+    MapGenerator& mMapGenerator;
+    TileMap& mTileMap;
+    EntityManager& mEntityManager;
+    SceneNode& mSceneGraph;
+
+    LevelTable& mLevelTable;
+    int mCurrentLevelIndex;
+    bool mLevelCompleted;
+    std::mt19937 mRandom;
+};
