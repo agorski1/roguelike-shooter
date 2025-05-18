bd95cafcea9a9bb5807338066a1a984e838fc6ac
Artur <artur.gorski5@wp.pl>
2025-05-18 23:29:28 +0200

    main

--- r1/Main.cpp
+++ r1/Main.cpp
@@ -1,37 +1,15 @@
-﻿#include <SFML/Graphics.hpp>
-
+﻿#include"Application.h"
+#include <SFML/Graphics.hpp>
+#include<iostream>
 int main()
 {
-    // Tworzenie okna o rozmiarze 800x600
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Działa!");
-
-    // Tworzenie kółka o promieniu 50
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Green); // Ustawienie koloru kółka na zielony
-
-    // Ustawienie pozycji kółka na środku okna
-    shape.setPosition(400 - 50, 300 - 50);
-
-    // Główna pętla programu
-    while (window.isOpen())
-    {
-        // Obsługa zdarzeń
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Czyszczenie okna
-        window.clear();
-
-        // Rysowanie kółka
-        window.draw(shape);
-
-        // Wyświetlenie zawartości okna
-        window.display();
-    }
-
-    return 0;
+	try
+	{
+		Application application;
+		application.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "\nEXCEPTION: " << e.what() << std::endl;
+	}
 }
